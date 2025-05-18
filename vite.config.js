90a43b9fe16d952688c66f4818365ad5221ae282
/Bimori <106877704+Bimori404@users.noreply.github.com>
2025-05-18 15:58:35 -0600

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
-  plugins: [vue()],
+  base: '/',
+  plugins: [
+    vue(),
+  ],
   server: {
-    host: '0.0.0.0', 
-    port: 5173,      
-    strictPort: true, 
+    host: '0.0.0.0'
   },
-});
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
+
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     host: '0.0.0.0', 
+//     port: 5173,      
+//     strictPort: true, 
+//   },
+// });
