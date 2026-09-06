f3a791d305fa7d2e3398137718e6312606d541e7
54M54M <jsamuelpgonzalez@gmail.com>
2026-09-05 21:08:34 -0600

    Integracion database neon - backend; Arreglo de bugs; Nombre actualizado

---
+++ b/ar-cs-tlahtolcalli/src/main/java/com/tlahtolli/api/config/Constants.java
@@ -0,0 +1,21 @@
+package com.tlahtolli.api.config;
+
+public final class Constants {
+
+    private Constants() {}
+
+    /*
+    // LOCAL
+    public static final String DB_URL      = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "12345";
+    public static final String DB_DRIVER   = "org.postgresql.Driver";
+    */
+
+    // DEV
+    public static final String DB_URL      = "jdbc:postgresql://ep-jolly-bar-ay6hx1la.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channelBinding=require";
+    public static final String DB_USERNAME = "neondb_owner";
+    public static final String DB_PASSWORD = "npg_2oI7brxgTvRX";
+    public static final String DB_DRIVER   = "org.postgresql.Driver";
+
+}
